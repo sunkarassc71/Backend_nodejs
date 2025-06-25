1cd2229f26b931f25074e6544af9bd85157ce38e
sunkarassc71 <120734051+sunkarassc71@users.noreply.github.com>
2025-06-25 18:14:59 +0530

    warning issue fix

--- index.js
+++ index.js
@@ -14,8 +14,6 @@ app.use('/api/users', userRoutes);
 
 // DB Connect
 mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
 })
 .then(() => console.log("✅ MongoDB connected"))
 .catch((err) => console.error("❌ MongoDB error:", err));
